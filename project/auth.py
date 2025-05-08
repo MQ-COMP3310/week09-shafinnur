719e409460f326e1c93090ee49a490a4a633c01e
nurs <shafinnur31@hotmail.com>
2025-05-08 10:45:34 +1000

    push secure

--- project/auth.py
+++ project/auth.py
@@ -38,8 +38,10 @@ def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+    
+    signup_query = text ('select * from user where email = :email')
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
+    user = db.session.execute (signup_query, = {'email' : email }).all()
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
